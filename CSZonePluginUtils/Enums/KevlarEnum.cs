895793b7132fd8d791d41677b36d6315f92bdaf3
Robert <lordfetznschaedl@gmail.com>
2024-02-04 19:45:17 +0100

    Added EnumMember with give string to KevlarEnum

--- CSZonePluginUtils/Enums/KevlarEnum.cs
+++ CSZonePluginUtils/Enums/KevlarEnum.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -10,8 +11,13 @@ namespace CSZoneNet.Plugin.Utils.Enums
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum KevlarEnum
     {
+        [EnumMember(Value = "")]
         None = 0,
+
+        [EnumMember(Value = "item_kevlar")]
         Kevlar = 1,
+
+        [EnumMember(Value = "item_assaultsuit")]
         KevlarHelmet = 2,
     }
 }
